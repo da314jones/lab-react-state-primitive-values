cb992ac626e3e497d9cf4a48687efc383dc05dea
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-07-30 17:59:46 -0400

    Second push

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,14 @@ function App () {
 
   const increaseIncrement = () => {
     if(score >= 10) {
-      setIncrement() = increment + 1;
+      setIncrement(increment + 1);
     }
   }
 
     return (
       <main>
         <h1>Current Score:</h1> 
-        <button onClick={increaseScore()}>X</button>
+        <button onClick={increaseScore}>+{increment}</button>
         <button onClick={payPoints}>Pay 10 points to change from X to X+1</button>
       </main>
     );

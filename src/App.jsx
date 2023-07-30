58f612f25353311c94d76eb553b647d1db559fc1
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-07-30 17:54:15 -0400

    Lab w/ Dwyane: Initial Push

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import "./App.css";
 
 function App () {
+
+  const [score, setScore] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  const increaseScore = () => {
+    setScore(score + increment);
+  }
+
+  const increaseIncrement = () => {
+    if(score >= 10) {
+      setIncrement() = increment + 1;
+    }
+  }
+
     return (
       <main>
-        <p>React State Lab</p>
+        <h1>Current Score:</h1> 
+        <button onClick={increaseScore()}>X</button>
+        <button onClick={payPoints}>Pay 10 points to change from X to X+1</button>
       </main>
     );
 }

4c57f1bded6c320552cdfbd93925d567173a5e9f
da314jones <dwaynejones@pursuit.org>
2023-07-31 23:01:36 -0400

    no styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-function App () {
-
+function App() {
   const [score, setScore] = useState(0);
   const [increment, setIncrement] = useState(1);
 
   const increaseScore = () => {
     setScore(score + increment);
-  }
+  };
 
-  const increaseIncrement = () => {
-    if(score >= 10) {
+  const payPoints = () => {
+    if (score < 10) {
+      alert("You cant afford that!");
+    } else {
+      setScore(score - 10);
       setIncrement(increment + 1);
     }
-  }
+  };
+  const replayGame = () => {
+    setScore(0);
+    setIncrement(1);
+  };
 
-    return (
-      <main>
-        <h1>Current Score:</h1> 
-        <button onClick={increaseScore}>+{increment}</button>
-        <button onClick={payPoints}>Pay 10 points to change from X to X+1</button>
-      </main>
-    );
+  return (
+    <main>
+      <h1>Current Score: {score}</h1>
+      {score < 100 ? (
+        <div>
+          <button onClick={increaseScore}>+{increment}</button>
+          <button onClick={payPoints}>
+            Pay 10 points to change from +{increment} to +{increment + 1}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>You Win!</h2>
+          <button onClick={replayGame}>Play again?</button>
+        </div>
+      )}
+      ;
+    </main>
+  );
 }
 
 export default App;
